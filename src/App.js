fa33af5c2982ebe394e2de459034bf3fb77c221f
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-14 12:22:00 -0600

    ALL FOR ONE AND ONE FOR ALL

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import Nav from "./Components/Nav/Nav";
 import "./App.css";
+import { HashRouter } from 'react-router-dom'
+import routes from './routes'
 
 class App extends Component {
   render() {
     return (
+      <HashRouter>
       <div className="App">
-        <Nav />
-        <h1 style={{ padding: "200px 35%" }}>
-          This is where your pages will appear
-        </h1>
+          <Nav />
+            {routes}
       </div>
+      </HashRouter>
     );
   }
 }
 
 export default App;
+
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import posts from "./../../post_data.json";
 import "./Post.css";
+import { parse } from "path";
 
 class Post extends Component {
   constructor(props) {
@@ -13,7 +14,7 @@ class Post extends Component {
   componentDidMount() {
     // This is where you would make an axios call to a server in a fullstack application
     // but for today we'll be just be filtering an array of dummy data
-    let post = posts.find(post => post.id === 2);
+    let post = posts.find(post => post.id === parseInt(this.props.match.params.id));
     this.setState({
       title: post.title,
       content: post.content
